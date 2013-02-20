bef883628ca628faf0164dd7bfcdfbf025fcd7f9
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-02-20 23:43:51 +0530

    Test bundle's bemdecl fixed

--- desktop.bundles/index/index.bemdecl.js
+++ desktop.bundles/index/index.bemdecl.js
@@ -1,3 +1,6 @@
 exports.blocks = [
-    { block : 'global', mods : { init : 'auto' } }
+    { block : 'global', mods : { init : 'auto' } },
+    { block : 'i-photos' },
+    { block : 'i-users', mods : { type : 'bulk' } },
+    { block : 'i-comments', mods : { type : 'bulk' } }
 ];
