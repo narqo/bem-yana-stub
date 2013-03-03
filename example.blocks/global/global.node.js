ba215d2c3d9b46d841cac7a231b75b8027015de0
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-03 14:17:54 +0400

    Use bem-server as static-view

--- example.blocks/global/global.node.js
+++ example.blocks/global/global.node.js
@@ -2,9 +2,11 @@ exports.main = main;
 
 function main() {
     var path = require('path'),
+        url = require('url'),
         join = path.join,
 
-        PRJ_ROOT = path.resolve(__dirname, '../../');
+        PRJ_ROOT = path.resolve(__dirname, '../../'),
+        BUNDLE_PATH = path.relative(PRJ_ROOT, __dirname) + path.sep;
 
     App.Config.params({
 
@@ -12,11 +14,12 @@ function main() {
 
         BUNDLES_ROOT : join(PRJ_ROOT, 'examples.bundles'),
 
+        STATIC_URL : url.resolve('http://127.0.0.1:8080/', BUNDLE_PATH),
         STATIC_ROOT  : __dirname,   // `STATIC_ROOT` is pointing to bundle's dir
 
         routes : [
             { rule : '/$', action : 'page' },
-            { rule : '/.+\.(?:css|js|ico)$', action : 'static' }
+//            { rule : '/.+\.(?:css|js|ico)$', action : 'static' }
         ]
 
     });
