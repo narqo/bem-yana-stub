0bf6a36f5aae0f7447e2813e2f4a19d26daf6e4c
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-04 16:28:43 +0400

    example runner fixed

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 function log(data) {
+    console.log('----->');
     console.log(JSON.stringify(data, null, 2));
 }
 
-require('./desktop.bundles/index/index.bemtree.xjst').BEMTREE.call({ d : 0 }, { callback : log });
+var json = require('./desktop.bundles/index/index.bemtree.xjst').BEMTREE.call({ d : 0 });
+
+json.then(log);
