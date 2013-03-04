6c06da5182d41f261fe7de69b1efa8c029476a5a
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-04 18:51:24 +0400

    Example runner fixed

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 function log(data) {
     console.log('----->');
     console.log(JSON.stringify(data, null, 2));
+
+    console.log(require('./desktop.bundles/index/index.bemhtml.js').BEMHTML.apply(data));
 }
 
 var json = require('./desktop.bundles/index/index.bemtree.xjst').BEMTREE.call({ d : 0 });
