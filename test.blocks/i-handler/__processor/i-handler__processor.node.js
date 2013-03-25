fc6c8bfbfe1e39d361532ac35a05541c0e0ab64d
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-25 14:57:34 +0400

    test.blocks \w i-handeler testing structures

---
+++ test.blocks/i-handler/__processor/i-handler__processor.node.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview Логгер процессора для тестов
+ */
+
+(function(global, BEM, Yana) {
+
+BEM.decl('i-handler__processor', {}, {
+
+    logger : Yana.Logger
+
+});
+
+}(this, BEM, Yana));
---
+++ test.blocks/i-handler/__request/i-handler__request.js
@@ -0,0 +1,11 @@
+(function(global, BEM) {
+
+BEM.decl('i-handler__request', {}, {
+
+    getProviderUrl : function() {
+        return 'http://localhost:3001/models';
+    }
+
+});
+
+}(this, BEM));
